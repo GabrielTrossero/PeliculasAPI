94e6591af13890f2ecb4d6dbf97be5ea6a69f233
Gabriel Trossero <gabi_trossero@hotmail.com>
2023-03-27 14:15:24 -0300

    visual changes

--- PeliculasFront - Bootstrap/src/app/components/navbar/navbar.component.ts
+++ PeliculasFront - Bootstrap/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  //Función que captura el scrolling
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    //capturo el navbar
+    let element = document.querySelector('.navbar') as HTMLElement;
+
+    //si scrolleo para abajo, le cambio el color
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar-scrolled');
+    } else {
+      element.classList.remove('navbar-scrolled');
+    }
+  }
+
   constructor(private router: Router) {}
 
   buscarPelicula(texto: string) {
